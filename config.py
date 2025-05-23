edd7bd7311fc25916e83aa7bff31dc1f7b1980bb
tjmnmk <adam.bambuch2@gmail.com>
2025-05-24 01:33:42 +0200

    logging

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import time
 import traceback
+import sys
 
 def config_loop(easy_speedfan):
     temp_cpu = easy_speedfan.sensors.TemperatureSensor("coretemp-isa-0000", "Package id 0", "temp1_input")
@@ -12,15 +13,15 @@ def config_loop(easy_speedfan):
     previous_pwm_value = 0
     try:
         while True:
-            print("Config loop")
+            print("Config loop", file=sys.stderr)
 
             # get the temperature
             temp_cpu_value = temp_cpu.get_temperature()
             temp_gpu_value = temp_gpu.get_temperature()
 
             # print the temperature
-            print(f"CPU temp: {temp_cpu_value}")
-            print(f"GPU temp: {temp_gpu_value}")
+            print(f"CPU temp: {temp_cpu_value}", file=sys.stderr)
+            print(f"GPU temp: {temp_gpu_value}", file=sys.stderr)
 
             pwm_value = easy_speedfan.pwm_calc.linear_pwm(temp_cpu_value, 50, 80, 75, 255)
             if temp_gpu_value > 100 and pwm_value < 170:
@@ -33,7 +34,7 @@ def config_loop(easy_speedfan):
             pwm_cpu2.set_pwm_value(pwm_value)
 
             # print the pwm value
-            print(f"PWM value: {pwm_value}")
+            print(f"PWM value: {pwm_value}", file=sys.stderr)
             # set the previous pwm value
             previous_pwm_value = pwm_value
             # sleep for the loop sleep time
@@ -41,13 +42,14 @@ def config_loop(easy_speedfan):
             time.sleep(1)
 
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error: {e}", file=sys.stderr)
         # print the stack trace
-        traceback.print_exc()
+        traceback.print_exc(file=sys.stderr)
 
     finally:
+        print("Error occurred, disabling fan control", file=sys.stderr)
         pwm_cpu2.disable_fan_control()
-        print("Fan control disabled")
+        print("Fan control disabled", file=sys.stderr)
 
         
         
